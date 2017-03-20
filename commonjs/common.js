201b7299496f10b4a9db884193c1d0a7bc0c9f31
lanlanzi <lanlanzi2@163.com>
2017-03-21 00:55:06 +0800

    V0.03

--- commonjs/common.js
+++ commonjs/common.js
@@ -36,21 +36,22 @@ $(document).ready(function() {
    // console.log($(".stmenu"))
     $(".stmenu").hover(function()
         {
-            // $('.children').stop(true,true).hide();
-            // $('.nav-dropdown').removeClass('navhover');
-            // $(".navhover-decrtion").css("display","none");
+            $('.children').stop(true,true).hide();
+            $(".navhover-decrtion").css("display","none");
+            $('.nav-dropdown').removeClass('navhover');
             $(this).find('.children').delay(200).animate({ opacity:'show', height:'show' },200);
-            $(this).find('.nav-dropdown').delay(200).addClass('navhover');
             $(this).find('.navhover-decrtion').delay(200).css("display","block");
+            $(this).find('.nav-dropdown').delay(200).addClass('navhover');
         },
         function()
         {
             $('.children').stop(true,true).hide();
-            $('.nav-dropdown').removeClass('navhover');
             $(".navhover-decrtion").css("display","none");
+            $('.nav-dropdown').removeClass('navhover');
         });
 
 
+//banner
 
 
 
