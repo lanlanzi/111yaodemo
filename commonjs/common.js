ebcde5b0223ada6eb1e4b27045c65ef0d9fb4e3f
lanlanzi <lanlanzi2@163.com>
2017-03-21 17:52:51 +0800

    v0.03

--- commonjs/common.js
+++ commonjs/common.js
@@ -36,25 +36,44 @@ $(document).ready(function() {
    // console.log($(".stmenu"))
     $(".stmenu").hover(function()
         {
+
             $('.children').stop(true,true).hide();
             $(".navhover-decrtion").css("display","none");
+            // $('.nav-dropdown').removeClass('navhover');
+            // if ($(this).index()==0){
+            //     $('.nav-dropdown').eq(0).css("color","black");
+            // }else{
+            //
+            // };
             $('.nav-dropdown').removeClass('navhover');
             $(this).find('.children').delay(200).animate({ opacity:'show', height:'show' },200);
             $(this).find('.navhover-decrtion').delay(200).css("display","block");
             $(this).find('.nav-dropdown').delay(200).addClass('navhover');
+            //$(this).find('.navhover').delay(200).css("color","#00aaa0");
         },
         function()
         {
             $('.children').stop(true,true).hide();
             $(".navhover-decrtion").css("display","none");
             $('.nav-dropdown').removeClass('navhover');
+            // if ($(this).index()!=0){
+            //     $('.nav-dropdown').css("color","black");
+            //     $('.nav-dropdown').eq(0).css("color","red");
+            // }else{
+            //     $('.nav-dropdown').eq(0).css("color","red");}
         });
-
-
 //banner
-
-
-
+    $('.swiper-container').hover(function(){
+        mySwiper.stopAutoplay();
+        $(".swiper-button-prev").show();
+        $(".swiper-button-next").show();
+        $(".swiper-pagination").show();
+    },function(){
+        mySwiper.startAutoplay();
+        $(".swiper-button-prev").hide();
+        $(".swiper-button-next").hide();
+        $(".swiper-pagination").hide();
+    });
 
 
 });
